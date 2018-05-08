015dd775b5f7c45602111f61ddbf8d1df5408fbe
张欢 <1213478506@qq.com>
2018-05-08 09:47:30 +0800

    Initial Commit

---
+++ KBDTestTool/KBDTestTool.h
@@ -0,0 +1,19 @@
+//
+//  KBDTestTool.h
+//  KBDTestTool
+//
+//  Created by mac on 2018/5/8.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KBDTestTool.
+FOUNDATION_EXPORT double KBDTestToolVersionNumber;
+
+//! Project version string for KBDTestTool.
+FOUNDATION_EXPORT const unsigned char KBDTestToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KBDTestTool/PublicHeader.h>
+
+
